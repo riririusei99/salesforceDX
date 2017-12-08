3feb89296cd8f4d0ace70efc939425e100ac2721
Ryusei Namai <riry1313@gmail.com>
2017-12-09 02:09:55 +0900

    3rd commit

--- plugins/response.py
+++ plugins/response.py
@@ -1,24 +1,24 @@
 from slackbot.bot import respond_to
-from slackbot.bot import listen_to
-import re
+from slackbot.bot import default_reply
 
-@respond_to('hi', re.IGNORECASE)
-def hi(message):
-    message.reply('I can understand hi or HI!')
-    # 絵文字のリアクション
-    message.react('+1')
+import pya3rt
+import jaconv
 
-@respond_to('I love you')
-def love(message):
-    message.reply('I love you too!')
+@respond_to('ジーク')
+def zeon(message):
+    message.reply('ジオン！！')
 
-@listen_to('Can someone help me?')
-def help(message):
-    # メンションをつけてくれます
-    message.reply('Yes, I can!')
-
-    # チャンネルに向けて発言してくれます
-    message.send('I can help everybody!')
-
-    # スレッドで返信
-    message.reply("Here's a threaded reply", in_thread=True)
+@default_reply()
+def talkA3rt(message):
+    apikey = ''
+    client = pya3rt.TalkClient(apikey)
+    api_response = client.talk(message.body['text']) 
+    # レスポンスがokの時返事を返す
+    if api_response['message'] == 'ok':
+        reply_message = api_response['results'][0]['reply']
+        message.reply(jaconv.hira2kata(reply_message) + '…ロボ')
+        # 普通の返事
+        # message.reply(reply_message)
+    # APIエラーの時はmessageを返す
+    else:
+        message.reply('エラー、ウマク返事ガデキマセン [ERROR:' + api_response['message'] + ']')
