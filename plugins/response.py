7c31342cb78fe9aa0adac363ae58f41a2774c07c
Ryusei Namai <riry1313@gmail.com>
2017-12-19 03:23:12 +0900

    wikipedia検索を追加

--- plugins/response.py
+++ plugins/response.py
@@ -3,16 +3,28 @@ from slackbot.bot import default_reply
 
 import pya3rt
 import jaconv
+import wikipedia
 
 @respond_to('ジーク')
 def zeon(message):
     message.reply('ジオン！！')
 
+@respond_to('(.*)について知りたい件')
+def searchWiki(message, something):
+    response_string = ''
+    wikipedia.set_lang('ja')
+    try:
+        wiki_page = wikipedia.page(something)
+        message.reply('なるほど、{0}についてですね。'.format(something))
+        message.reply(wiki_page.url)
+    except wikipedia.exceptions.DisambiguationError  as e:
+        message.reply('エラー、wikiの結果が上手く返ってきませんでした。')
+
 @default_reply()
 def talkA3rt(message):
     apikey = ''
     client = pya3rt.TalkClient(apikey)
-    api_response = client.talk(message.body['text']) 
+    api_response = client.talk(message.body['text'])
     # レスポンスがokの時返事を返す
     if api_response['message'] == 'ok':
         reply_message = api_response['results'][0]['reply']
